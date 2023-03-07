f254db0114bc485e9aa9f799f648f6e3a76a151d
aljiza21 <61503951+aljiza21@users.noreply.github.com>
2023-03-07 19:45:28 +0800

    Update BookDatabase.java

--- designPattern_Activities/src/BookDatabase.java
+++ designPattern_Activities/src/BookDatabase.java
@@ -1,35 +1,28 @@
 import java.util.HashMap;
 import java.util.Map;
 public class BookDatabase {
+    private BookDatabase() {}
     private static BookDatabase instance;
-    Map<Integer, String> bookMap;
-    private int bookId;
+    private Map<Integer, String> bookMap = new HashMap<>();
+    private int bookId = 1;
 
-    public BookDatabase() {
-        bookMap = new HashMap<>();
-        bookId = 1;
-    }
     public static BookDatabase getInstance() {
         if (instance == null) {
             instance = new BookDatabase();
         }
         return instance;
     }
-
     public void addBook(String [] bookName){
         for (String book : bookName) {
-            this.bookMap.put(bookId, book);
+            bookMap.put(bookId, book);
             bookId++;
         }
-
     }
-
     public void viewBook(){
         for (Map.Entry<Integer, String> entry : bookMap.entrySet()) {
             System.out.println("Book " + entry.getKey() + ": " + entry.getValue());
         }
     }
-
     public void removeBook(int bookId){
         if (bookMap.containsKey(bookId)) {
             System.out.println("The Book '" + bookMap.remove(bookId) + "' is remove.");
